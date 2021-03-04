8aed3bf68cee57b4c83800f0c42eb7ac5d865372
garcyv <garcyv@gmail.com>
2021-03-04 17:54:19 -0300

    Añade viemodel a FirstFragment

--- app/src/main/java/com/example/anchorbooks_sem1/FirstFragment.kt
+++ app/src/main/java/com/example/anchorbooks_sem1/FirstFragment.kt
@@ -1,28 +1,41 @@
 package com.example.anchorbooks_sem1
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.example.anchorbooks_sem1.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+    private lateinit var binding: FragmentFirstBinding
+    private val viewModel: BooksViewModel by activityViewModels()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getFetchBooksWithCoroutines().observe(viewLifecycleOwner, Observer {
+            it?.let {
+                Log.d("LISTADO COR",it.toString())
+            }
+        })
 
         view.findViewById<Button>(R.id.button_first).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
